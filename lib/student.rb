faad9e8c0ad53fd1443fecfddf6b1521d96b8db7
Brian Nicholls <nichol88@gmail.com>
2020-01-22 14:49:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,12 +10,16 @@ class Student
   end
 
   def self.create_table
-    <<-SQL
+    sql <<-SQL
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
     )
     SQL
+  end
+
+  def self.drop_table
+    
   end
 end
